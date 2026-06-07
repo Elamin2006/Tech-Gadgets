3fc6c095eeec327aefee1d08718bb501faf11a32
Elamin2006 <elamin2006@gmail.com>
2026-06-07 15:25:11 +0300

    feat: connect mongoDB Atlas

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import { DBConnection } from "./Database/mongoose.js";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+async function startServer() {
+  try {
+    await DBConnection();
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.error("Error starting server:", error);
+  }
+}
+
+startServer();
