36ef2145b2e8e18381f5958053658acd3727a61b
Elamin2006 <elamin2006@gmail.com>
2026-06-12 14:42:49 +0300

    feat(product): create product schema with timestamps and financial validation

---
+++ server/Model/Product.model.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    name: {
+        type: String,
+        required: [true, "Product name is required"],
+        maxlength: [100, "Product name cannot exceed 100 characters"]
+    },
+    description: {
+        type: String,
+        required: [true, "Product description is required"],
+        trim: true
+    },
+    price: {
+        type: Number,
+        required: [true, "Product price is required"],
+        min: [0, "Product price cannot be negative"]
+    },
+    image: {
+        type: String,
+        required: [true, "Product image URL is required"],
+    },
+    discount: {
+        type: Number,
+        default: 0,
+        min: [0, "Discount cannot be less than 0"],
+        max: [100, "Discount cannot exceed 100%"]
+    },
+    quantity: {
+        type: Number,
+        required: [true, "Product quantity is required"],
+        default: 1,
+        min: [0, "Product quantity cannot be negative"]
+    },
+    categoryId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Category"
+    }
+
+}
+    , { timestamps: true });
+
+const Product = mongoose.model('Product', productSchema);
+export default Product;
