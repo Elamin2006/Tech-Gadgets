330b073e2b1073c909ee817d7c73bc84add965ed
Elamin2006 <elamin2006@gmail.com>
2026-06-13 18:27:56 +0300

    refactor(auth): remove redundant auth.validation wrapper and apply inline generic validatorMiddleware

--- server/Routes/user.routes.js
+++ server/Routes/user.routes.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import {register, login, forgotPassword, verifyResetCode, resetPassword} from '../Controllers/user.controller.js'
-import { validateRegister, validateLogin } from '../Middlewares/auth.validation.js';
-
+import validationMiddleware from '../Middlewares/validation.middleware.js';
+import { registerValidation, loginValidation } from '../validations/user.validation.js';
 const userRouter = express.Router();
 
-userRouter.post("/register", validateRegister, register);
-userRouter.post("/login", validateLogin, login);
+userRouter.post("/register", validationMiddleware(registerValidation), register);
+userRouter.post("/login", validationMiddleware(loginValidation), login);
 userRouter.route('/forgot-password').post(forgotPassword)
 userRouter.route('/verify-reset-code').post(verifyResetCode)
 userRouter.route('/reset-password').post(resetPassword)
