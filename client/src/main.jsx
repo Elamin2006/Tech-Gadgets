fb19effc3560c7af85ba06af62c6b14b253cc0d1
Elamin2006 <elamin2006@gmail.com>
2026-06-22 14:36:58 +0300

    chore(config): globalize custom palette in index.css and main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import { store } from "./store/store";   
 import App from "./App";
 import "./index.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
