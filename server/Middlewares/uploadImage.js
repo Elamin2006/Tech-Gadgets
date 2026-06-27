927df81a555e68e8f7cf98e4678553c9e4d03ba2
Elamin2006 <elamin2006@gmail.com>
2026-06-27 13:05:01 +0300

    chore: remove legacy local uploads static handling and update server routes for cloudinary configuration

--- server/Middlewares/uploadImage.js
+++ server/Middlewares/uploadImage.js
@@ -1,19 +1,27 @@
 import multer from "multer";
-import path from 'path';
+import { v2 as cloudinary } from "cloudinary"; 
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 import ApiError from "../Utils/apiError.js";
+import dotenv from "dotenv";
 
-const imageStorage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'Uploads/'); 
+dotenv.config();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const cloudinaryStorage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: "tech_gadgets_products", 
+        allowed_formats: ["jpg", "jpeg", "png", "webp"], 
     },
-    filename: function (req, file, cb) {
-        const extension = path.extname(file.originalname); 
-        cb(null, `${Date.now()}${extension}`);
-    }
 });
 
 const imageUpload = multer({
-    storage: imageStorage,
+    storage: cloudinaryStorage,
     limits: { fileSize: 1024 * 1024 * 5 },  
     fileFilter: (req, file, cb) => {
         if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
