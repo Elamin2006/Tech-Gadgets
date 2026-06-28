15420cdac78e0a1d172c789ea2230deabca21190
Elamin2006 <elamin2006@gmail.com>
2026-06-28 17:45:10 +0300

    refactor(navbar): integrate global redux cart state for live badge counter rendering

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ export default function NavBar() {
   const navigate = useNavigate();
 
   const { user } = useSelector((state) => state.auth);
-  const { cartList } = useSelector((state) => state.cart) || { cartList: [] };
+  const { cartList, numOfCartItems, totalCartPrice } = useSelector((state) => state.cart) || { cartList: [] };
 
   const [expanded, setExpanded] = useState(false);
   const [isFixed, setIsFixed] = useState(false);
@@ -61,7 +61,11 @@ export default function NavBar() {
               data-num={cartList.length}
             >
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="cart-icon">
-                <path d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
+                <path d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 
+                0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75
+                 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 
+                 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 
+                 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
               </svg>
             </Link>
           </div>
@@ -78,18 +82,27 @@ export default function NavBar() {
             
             <Nav className="nav-links d-flex m-auto">
             <Nav.Item>
-              <Link aria-label="Go to Home Page" className="elite-nav-link" to="/" onClick={() => setExpanded(false)}>
+              <Link aria-label="Go to Home Page" 
+              className="elite-nav-link" to="/" 
+              onClick={() => setExpanded(false)}
+              >
                 Home
               </Link>
             </Nav.Item>
 
             <Nav.Item>
-              <Link aria-label="Go to Shop Page" className="elite-nav-link" to="/shop" onClick={() => setExpanded(false)}>
+              <Link aria-label="Go to Shop Page" 
+              className="elite-nav-link" to="/shop" 
+              onClick={() => setExpanded(false)}
+              >
                 Shop
               </Link>
             </Nav.Item>
             <Nav.Item>
-              <Link aria-label="Go to Shop Page" className="elite-nav-link" to="/shop" onClick={() => setExpanded(false)}>
+              <Link aria-label="Go to Shop Page" 
+              className="elite-nav-link" to="/shop" 
+              onClick={() => setExpanded(false)}
+              >
                 Cart
               </Link>
             </Nav.Item>
@@ -99,7 +112,11 @@ export default function NavBar() {
             <Nav.Item className="expanded-cart">
               <Link aria-label="Go to Cart Page" to="/cart" className="cart-wrapper" data-num={cartList.length}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="cart-icon">
-                  <path d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
+                  <path d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752
+                   0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 
+                   15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864
+                    60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0
+                     113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
                 </svg>
               </Link>
             </Nav.Item>
@@ -120,12 +137,19 @@ export default function NavBar() {
             ) : (
               <>
                 <Nav.Item className="ms-md-2 d-flex justify-content-sm-center">
-                  <Button as={Link} to="/login" variant="link" className="btn-signin-elite text-decoration-none p-0 pe-md-2" onClick={() => setExpanded(false)}>
+                  <Button as={Link} to="/login" variant="link" 
+                  className="btn-signin-elite text-decoration-none p-0 pe-md-2" 
+                  onClick={() => setExpanded(false)}
+                  >
                     Sign In
                   </Button>
                 </Nav.Item>
                 <Nav.Item>
-                  <Button as={Link} to="/register" className="px-3 py-2 rounded-3 btn-signup-elite" onClick={() => setExpanded(false)}>
+                  <Button 
+                  as={Link} to="/register" 
+                  className="px-3 py-2 rounded-3 btn-signup-elite" 
+                  onClick={() => setExpanded(false)}
+                  >
                     Sign Up
                   </Button>
                 </Nav.Item>
