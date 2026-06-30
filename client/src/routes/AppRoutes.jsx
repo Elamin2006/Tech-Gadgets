427c068f90035031a6fb0b3d815bb1ef2cfdfca1
Elamin2006 <elamin2006@gmail.com>
2026-06-30 12:53:49 +0300

    feat(ux): implement react-toastify global alert management system

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useEffect } from "react"; 
-import { useDispatch } from "react-redux"; 
-import { fetchCart } from "../store/slices/cartSlice"; 
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchCart } from "../store/slices/cartSlice";
+import { ToastContainer } from "react-toastify";
 
 import Home from "../pages/Home.jsx";
 import Login from "../pages/Login/Login.jsx";
@@ -13,30 +14,48 @@ import ProtectedRoutes from "./ProtectedRoutes.jsx";
 import Shop from "../pages/Shop/Shop";
 
 export default function AppRoutes() {
-
-    const dispatch = useDispatch(); 
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (token) {
-      dispatch(fetchCart()); 
+      dispatch(fetchCart());
     }
   }, [dispatch]);
   return (
     <Router>
-        <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/shop" element={<Shop />} />
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        toastClassName="elite-toast"
+      />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/shop" element={<Shop />} />
 
-                <Route element={<ProtectedRoutes />}>
-                    <Route path="/cart" element={<Cart />} />
-                </Route>
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/cart" element={<Cart />} />
+        </Route>
 
-                <Route path="*" element={<h2 style={{ textAlign: "center", marginTop: "50px" }}>404 - Page Not Found </h2>} />
-            </Routes>
-        </Router>
-  )
+        <Route
+          path="*"
+          element={
+            <h2 style={{ textAlign: "center", marginTop: "50px" }}>
+              404 - Page Not Found{" "}
+            </h2>
+          }
+        />
+      </Routes>
+    </Router>
+  );
 }
