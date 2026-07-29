8112137272f5a02f0254e5d0179ddecde74ac735
Elamin2006 <elamin2006@gmail.com>
2026-07-29 12:07:02 +0300

    fix(app): skip cart hydration for admin sessions

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,26 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { ToastContainer } from "react-toastify";
+import { fetchCart } from "./store/slices/cartSlice";
+import { AuthService } from "./services/auth.service";
 import AppRoutes from "./routes/AppRoutes";
 
 export default function App() {
-    return (
-        <>
-            <AppRoutes />
-        </>
-    );
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    const isAdmin = user?.role === "admin";
+    
+    if (AuthService.isAuthenticated() && !isAdmin) {
+      dispatch(fetchCart());
+    }
+  }, [dispatch, user]);
+
+  return (
+    <>
+      <ToastContainer theme="dark" toastClassName="elite-toast" />
+      <AppRoutes />
+    </>
+  );
 }
