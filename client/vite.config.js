a696a588d9a961ab9adb6c20468e08a6a653db8a
Elamin2006 <elamin2006@gmail.com>
2026-07-30 12:35:07 +0300

    build(vite): add bundle visualizer and configure vendor manual chunks

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,52 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    
+    visualizer({
+      filename: 'dist/stats.html',
+      open: false,       
+      gzipSize: true,    
+      brotliSize: true,  
+      template: 'treemap',
+    }),
+  ],
+
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react-dom') || id.includes('react/')) {
+              return 'vendor-react';
+            }
+            if (id.includes('react-router')) {
+              return 'vendor-router';
+            }
+            if (id.includes('@reduxjs') || id.includes('react-redux') || id.includes('/redux/')) {
+              return 'vendor-redux';
+            }
+            if (id.includes('react-bootstrap') || id.includes('/bootstrap/')) {
+              return 'vendor-bootstrap';
+            }
+            if (id.includes('react-select')) {
+              return 'vendor-select';
+            }
+            if (id.includes('swiper')) {
+              return 'vendor-swiper';
+            }
+            if (id.includes('react-toastify')) {
+              return 'vendor-toast';
+            }
+            if (id.includes('/axios/')) {
+              return 'vendor-axios';
+            }
+          }
+        },
+      },
+    },
+  },
+});
