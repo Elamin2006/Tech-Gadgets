77355ef4a977dee9bf86a07eb667083b90c69bac
Elamin2006 <elamin2006@gmail.com>
2026-08-19 09:36:24 +0200

    refactor: use Cloudinary service in upload middleware

--- server/src/middlewares/uploadImage.ts
+++ server/src/middlewares/uploadImage.ts
@@ -1,14 +1,8 @@
 import multer from "multer";
-import { v2 as cloudinary } from "cloudinary";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 
 import ApiError from "../utils/apiError.js";
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+import { cloudinary } from "../services/cloudinary.js";
 
 const storage = new CloudinaryStorage({
   cloudinary,
@@ -20,6 +14,7 @@ const storage = new CloudinaryStorage({
 
 const uploadImage = multer({
   storage,
+
   fileFilter: (req, file, cb) => {
     const allowedMimeTypes = [
       "image/jpeg",
