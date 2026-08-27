64af7bb95d082d15a586db21a32ff6b789de1730
Elamin2006 <elamin2006@gmail.com>
2026-08-27 15:05:42 +0200

    feat(product): add search, review endpoints

--- server/src/routes/product.routes.ts
+++ server/src/routes/product.routes.ts
@@ -1,44 +1,177 @@
-import express from 'express';
+import express from "express";
+
 import {
-    createProduct,
-    deleteProduct,
-    getAllProducts,
-    getProductById,
-    updateProduct
-} from '../controllers/product.controller.js';
+  addReview,
+  createProduct,
+  deleteProduct,
+  deleteReview,
+  getAllProducts,
+  getProductById,
+  getProductReviews,
+  searchProducts,
+  updateProduct,
+} from "../controllers/product.controller.js";
 
-import authentication from '../middlewares/authentication.js';
-import { allowedTo } from '../middlewares/authorization.js';
-import imageUpload from '../middlewares/uploadImage.js';
+import authentication from "../middlewares/authentication.js";
+import { allowedTo } from "../middlewares/authorization.js";
+import uploadImage from "../middlewares/uploadImage.js";
+import validationMiddleware from "../middlewares/validation.middleware.js";
 
-import validatorMiddleware from '../middlewares/validation.middleware.js';
-import { createProductSchema, updateProductSchema } from '../validators/product.validation.js';
+import {
+  addReviewSchema,
+  createProductSchema,
+  productIdSchema,
+  productSearchSchema,
+  reviewIdSchema,
+  updateProductSchema,
+} from "../validators/product.validation.js";
 
-const productRouter = express.Router();
+const productRouter =
+  express.Router();
 
-productRouter.route('/')
-    .get(getAllProducts);
+/*
+ * Public routes
+ */
 
-productRouter.route('/:id')
-    .get(getProductById);
+productRouter.get(
+  "/",
+  validationMiddleware(
+    productSearchSchema,
+    "query",
+  ),
+  getAllProducts,
+);
 
-productRouter.use(authentication);
+productRouter.get(
+  "/search",
+  validationMiddleware(
+    productSearchSchema,
+    "query",
+  ),
+  searchProducts,
+);
 
-productRouter.route('/')
-    .post(
-        allowedTo('admin'),
-        imageUpload.single('image'),
-        validatorMiddleware(createProductSchema),
-        createProduct
-    );
+productRouter.get(
+  "/:id/reviews",
+  validationMiddleware(
+    productIdSchema,
+    "params",
+  ),
+  getProductReviews,
+);
 
-productRouter.route('/:id')
-    .patch(
-        allowedTo('admin'),
-        imageUpload.single('image'),
-        validatorMiddleware(updateProductSchema),
-        updateProduct
-    )
-    .delete(allowedTo('admin'), deleteProduct);
+productRouter.get(
+  "/:id",
+  validationMiddleware(
+    productIdSchema,
+    "params",
+  ),
+  getProductById,
+);
+
+/*
+ * Protected routes
+ */
+
+productRouter.use(
+  authentication,
+);
+
+/*
+ * Admin: create
+ */
+
+productRouter.post(
+  "/",
+  allowedTo("admin"),
+
+  uploadImage.array(
+    "images",
+    5,
+  ),
+
+  validationMiddleware(
+    createProductSchema,
+    "body",
+  ),
+
+  createProduct,
+);
+
+/*
+ * Admin: update
+ */
+
+productRouter.patch(
+  "/:id",
+  allowedTo("admin"),
+
+  uploadImage.array(
+    "images",
+    5,
+  ),
+
+  validationMiddleware(
+    productIdSchema,
+    "params",
+  ),
+
+  validationMiddleware(
+    updateProductSchema,
+    "body",
+  ),
+
+  updateProduct,
+);
+
+/*
+ * Customer/Admin: add review
+ */
+
+productRouter.post(
+  "/:id/reviews",
+  validationMiddleware(
+    productIdSchema,
+    "params",
+  ),
+
+  validationMiddleware(
+    addReviewSchema,
+    "body",
+  ),
+
+  addReview,
+);
+
+/*
+ * Admin: delete product
+ */
+
+productRouter.delete(
+  "/:id",
+  allowedTo("admin"),
+
+  validationMiddleware(
+    productIdSchema,
+    "params",
+  ),
+
+  deleteProduct,
+);
+
+/*
+ * Customer/Admin: delete review
+ */
+
+productRouter.delete(
+  "/:id/reviews/:reviewId",
+
+  validationMiddleware(
+    reviewIdSchema,
+    "params",
+  ),
+
+  deleteReview,
+);
 
 export default productRouter;
