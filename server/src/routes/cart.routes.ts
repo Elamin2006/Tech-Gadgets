55890601f30700cb7eb45b09324a40d97661959f
Elamin2006 <elamin2006@gmail.com>
2026-08-29 14:50:22 +0200

    feat(cart): apply coupon remove coupon end points

--- server/src/routes/cart.routes.ts
+++ server/src/routes/cart.routes.ts
@@ -1,35 +1,70 @@
 import express from "express";
+
 import {
   addToCart,
   getLoggedUserCart,
   updateCartItemQuantity,
   removeFromCart,
   clearCart,
+  applyCoupon,
+  removeCoupon,
 } from "../controllers/cart.controller.js";
 
 import authentication from "../middlewares/authentication.js";
 
-import validatorMiddleware from "../middlewares/validation.middleware.js";
+import validationMiddleware from "../middlewares/validation.middleware.js";
+
 import {
   addToCartSchema,
-  updateCartItemQuantitySchema,
+  updateCartItemSchema,
+  cartProductIdSchema,
+  applyCouponSchema,
 } from "../validators/cart.validation.js";
 
 const cartRouter = express.Router();
 
 cartRouter.use(authentication);
 
-cartRouter
-  .route("/")
-  .get(getLoggedUserCart)
-  .post(validatorMiddleware(addToCartSchema), addToCart)
-  .delete(clearCart);
-cartRouter
-  .route("/:itemId")
-  .patch(
-    validatorMiddleware(updateCartItemQuantitySchema),
-    updateCartItemQuantity,
-  )
-  .delete(removeFromCart);
+cartRouter.get(
+  "/",
+  getLoggedUserCart,
+);
+
+cartRouter.post(
+  "/items",
+  validationMiddleware(addToCartSchema),
+  addToCart,
+);
+
+cartRouter.patch(
+  "/items",
+  validationMiddleware(updateCartItemSchema),
+  updateCartItemQuantity,
+);
+
+cartRouter.delete(
+  "/items/:productId",
+  validationMiddleware(
+    cartProductIdSchema,
+    "params",
+  ),
+  removeFromCart,
+);
+
+cartRouter.delete(
+  "/clear",
+  clearCart,
+);
+
+cartRouter.post(
+  "/coupon",
+  validationMiddleware(applyCouponSchema),
+  applyCoupon,
+);
+
+cartRouter.delete(
+  "/coupon",
+  removeCoupon,
+);
 
 export default cartRouter;
