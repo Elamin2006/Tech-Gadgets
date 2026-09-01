ee07426d5bdb954ede42a4923d8aa3c4421bc03b
Elamin2006 <elamin2006@gmail.com>
2026-09-01 15:19:51 +0200

    feat(order): regiter the new endpoints

--- server/src/routes/order.routes.ts
+++ server/src/routes/order.routes.ts
@@ -1,31 +1,108 @@
 import express from "express";
+
+import authentication from "../middlewares/authentication.js";
+
+import {
+  allowedTo,
+} from "../middlewares/authorization.js";
+
+import validatorMiddleware from "../middlewares/validation.middleware.js";
+
+import {
+  createOrderSchema,
+  getAllOrdersSchema,
+  getMyOrdersSchema,
+  orderIdParamsSchema,
+  updateOrderStatusSchema,
+} from "../validators/order.validation.js";
+
 import {
   createCashOrder,
   getAllOrders,
+  getMyOrders,
   getOrderById,
   updateOrderStatus,
   deleteOrderById,
+  cancelMyOrder,
 } from "../controllers/order.controller.js";
 
-import authentication from "../middlewares/authentication.js";
-import { allowedTo } from "../middlewares/authorization.js";
-import validatorMiddleware from "../middlewares/validation.middleware.js";
+const orderRouter =
+  express.Router();
 
-import { createCashOrderSchema } from "../validators/order.validation.js";
-
-const orderRouter = express.Router();
-
-orderRouter.use(authentication);
+orderRouter.use(
+  authentication,
+);
 
 orderRouter
   .route("/")
-  .post(validatorMiddleware(createCashOrderSchema), createCashOrder)
-  .get(getAllOrders);
+  .post(
+    validatorMiddleware(
+      createOrderSchema,
+    ),
+    createCashOrder,
+  )
+  .get(
+    validatorMiddleware(
+      getAllOrdersSchema,
+      "query",
+    ),
+    getAllOrders,
+  );
+
+orderRouter
+  .route("/my-orders")
+  .get(
+    validatorMiddleware(
+      getMyOrdersSchema,
+      "query",
+    ),
+    getMyOrders,
+  );
+
+orderRouter
+  .route("/my/:orderId/cancel")
+  .patch(
+    validatorMiddleware(
+      orderIdParamsSchema,
+      "params",
+    ),
+    cancelMyOrder,
+  );
 
 orderRouter
   .route("/:orderId")
-  .get(getOrderById) 
-  .patch(allowedTo("admin"), updateOrderStatus) 
-  .delete(allowedTo("admin"), deleteOrderById); 
-  
+  .get(
+    validatorMiddleware(
+      orderIdParamsSchema,
+      "params",
+    ),
+    getOrderById,
+  )
+
+  .patch(
+    allowedTo("admin"),
+
+    validatorMiddleware(
+      orderIdParamsSchema,
+      "params",
+    ),
+
+    validatorMiddleware(
+      updateOrderStatusSchema,
+    ),
+
+    updateOrderStatus,
+  )
+
+  .delete(
+    allowedTo("admin"),
+
+    validatorMiddleware(
+      orderIdParamsSchema,
+      "params",
+    ),
+
+    deleteOrderById,
+  );
+
 export default orderRouter;
